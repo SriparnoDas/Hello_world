f031929973d68f54b33dba50ec2d284f94ea98fa
SriparnoDas <122005777+SriparnoDas@users.noreply.github.com>
2023-04-27 13:09:40 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Welcome world";
+        return "Welcome ";
     }
 
 }
